fc2a9c81c26e90896fd861e22e4100e1fe86147b
Phalente <phalenterobyn@gmail.com>
2023-03-17 15:58:27 +0100

    ⚡️ Improve slider for responsive

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,5 +1,5 @@
 let counter = 0;
-let timer, sliderItems, slides, slideWidth, sliderItemTitle;
+let timer, sliderItems, slides, slideWidth;
 
 window.onload = () => {
   const containerSlider = document.querySelector(".container-slider");
@@ -12,23 +12,30 @@ window.onload = () => {
   slides = Array.from(sliderItems.children);
 
   slideWidth = sliderItem.getBoundingClientRect().width;
+  console.log(slideWidth);
+
   let next = document.querySelector("#nav-right");
   let prev = document.querySelector("#nav-left");
 
   next.addEventListener("click", slideNext);
   prev.addEventListener("click", slidePrev);
+
   timer = setInterval(slideNext, 3500);
+
   containerSlider.addEventListener("mouseover", () => {
     stopTimer();
   });
   containerSlider.addEventListener("mouseout", () => {
     startTimer();
   });
+
   function slideNext() {
     counter++;
     sliderItems.style.transition = "1s linear";
+
     let decal = -slideWidth * counter;
     sliderItems.style.transform = `translateX(${decal}px)`;
+
     setTimeout(function () {
       if (counter >= slides.length - 1) {
         counter = 0;
@@ -54,9 +61,11 @@ window.onload = () => {
     let decal = -slideWidth * counter;
     sliderItems.style.transform = `translateX(${decal}px)`;
   }
+
   function stopTimer() {
     clearInterval(timer);
   }
+
   function startTimer() {
     timer = setInterval(slideNext, 2000);
   }
